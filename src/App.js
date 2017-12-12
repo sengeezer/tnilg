c79b2203bd271960f35cf66ed37ead85f1ffa1f5
Fabio Albertin <fabio.albertin@matchesfashion.com>
2017-12-12 18:16:23 +0000

    Added Redux and grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './App.css';
@@ -9,7 +9,20 @@ class App extends Component {
     return (
       <div>
         <Grid>
-          <h1>Glint Coding Challenge</h1>
+          <Row>
+            <h1>Glint Coding Challenge</h1>
+          </Row>
+          <Row>
+            <Col xs={6} md={4}>
+              <Row>
+                Search
+              </Row>
+              <Row>
+                Feed list
+              </Row>
+            </Col>
+            <Col xs={12} md={8}>Main pane</Col>
+          </Row>
         </Grid>
       </div>
     );
