e75674c4939114c52031a3560f6beab6f56a6e0c
Fabio Albertin <albertin@gmail.com>
2017-12-27 21:57:26 +0100

    Added Main component

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h2>How to use</h2>
+      <ol>
+        <li>To add a feed, enter the URL of a Reddit JSON feed into the URL bar and hit Submit.</li>
+        <li>
+          To select a feed, click on its URL in the list below the URL field. If you only
+          have one feed in the list, its posts will appear here by default.
+        </li>
+      </ol>
+    </div>
+  );
+};
