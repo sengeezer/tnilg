d89c319b722140b6bb4796293f94e7310fbd7183
Fabio Albertin <albertin@gmail.com>
2018-01-09 23:44:48 +0000

    Used marked to render posts as HTML

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import marked from 'marked';
 import { Panel } from 'react-bootstrap';
 
 export default (props) => {
   return (
     <Panel header={<h4><strong>{props.title}</strong><span> - {props.date}</span></h4>}>
-      <p>{props.content}</p>
+      <div dangerouslySetInnerHTML={{__html: marked(props.content)}} />
     </Panel>
   );
 };
