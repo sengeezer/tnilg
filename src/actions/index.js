55134573cbc14fc03b3749add35a415ae2bfd760
Fabio Albertin <albertin@gmail.com>
2018-01-22 23:27:58 +0000

    Started switch to feedly cloud

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
+import keys from '../config/keys';
+
+const API_KEY = keys.feedlyAccess;
+const DOMAIN = 'http://cloud.feedly.com';
+const ROOT_URL = `${DOMAIN}/v3/streams/contents?streamId=feed`;
 
 export const FETCH_FEED = 'FETCH_FEED';
 
 export function fetchFeed(feedURl) {
-  const req = axios.get(feedURl);
+  // const url = `${ROOT_URL}/${feedURl}&n=20&access_token=${API_KEY}`;
+  const url = `${ROOT_URL}/${feedURl}&n=20`;
+  console.log('url', url);
+  const req = axios.get(url, {
+    // withCredentials: true,
+    Authorization: `OAuth ${API_KEY}`,
+  });
 
   return {
     type: FETCH_FEED,
